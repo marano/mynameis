e30ef8512440c50a56390a652b8eb39e3639a595
marano <thiagomarano@gmail.com>
2014-09-04 11:29:34 -0300

    Removes hover and selected from world tile.

--- js/WorldTile.js
+++ js/WorldTile.js
@@ -3,25 +3,7 @@ function WorldTile(world, x, y) {
   this.x = x;
   this.y = y;
   this.worldObjects = [];
-  this.hovered = false;
-  this.selected = false;
 }
-
-WorldTile.prototype.setSelected = function () {
-  this.selected = true;
-};
-
-WorldTile.prototype.setUnselected = function () {
-  this.selected = false;
-};
-
-WorldTile.prototype.setHovered = function () {
-  this.hovered = true;
-};
-
-WorldTile.prototype.unsetHovered = function () {
-  this.hovered = false;
-};
 
 WorldTile.prototype.uiElements = function () {
   return _(this.worldObjects).map(function (worldObject) {
