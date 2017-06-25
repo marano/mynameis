5b5d803673d83b15ba96143a2460c33033c91346
marano <thiagomarano@gmail.com>
2017-06-25 21:50:10 +0700

    Centralize viewport content on screen.

--- js/components/Viewport.js
+++ js/components/Viewport.js
@@ -4,20 +4,37 @@ import { state } from 'cerebral/tags';
 
 import WorldTile from './WorldTile';
 
-function Viewport({ tilesIndexes }) {
+function Viewport({ tilesIndexes, tileSize, worldDimension }) {
   return (
-    <viewport style={style()}>
-      {tilesIndexes.map((tileIndex) => <WorldTile tileIndex={tileIndex} />)}
-    </viewport>
+    <div style={wrapperStyle()}>
+      <div style={containerStyle()}>
+        {tilesIndexes.map((tileIndex) => <WorldTile tileIndex={tileIndex} />)}
+      </div>
+    </div>
   );
 
-  function style() {
+  function wrapperStyle() {
     return {
-      position: 'relative'
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+    };
+  }
+
+  function containerStyle() {
+    return {
+      position: 'relative',
+      width: worldDimension.width * tileSize,
+      height: worldDimension.height * tileSize
     };
   }
 }
 
 export default connect({
-  tilesIndexes: state`world.tiles.*`
+  tilesIndexes: state`world.tiles.*`,
+  tileSize: state`viewport.tileSize`,
+  worldDimension: state`world.dimension`
 }, Viewport);
