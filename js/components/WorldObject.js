f96c7e29a993203213cc96792dd2cebe57aaec54
marano <thiagomarano@gmail.com>
2017-09-21 00:42:48 -0300

    Position selection cursor inside viewport when element is at border.

--- js/components/WorldObject.js
+++ js/components/WorldObject.js
@@ -1,6 +1,9 @@
+import { map, filter } from 'lodash' ;
 import { linkEvent } from 'inferno';
 import { connect } from '@cerebral/inferno';
 import { props, state, signal } from 'cerebral/tags';
+
+import { computeIsElementAtAxis } from '../computes';
 
 import UiElement from './UiElement';
 
@@ -8,6 +11,10 @@ export default connect({
   uiElemetsIndexes: state`${props`sceneDataPath`}.worldObjects.${props`worldObjectId`}.uiElements.*`,
   zIndex: state`${props`sceneDataPath`}.worldObjects.${props`worldObjectId`}.zIndex`,
   isSelected: state`${props`sceneDataPath`}.worldObjects.${props`worldObjectId`}.isSelected`,
+  isAtTopBorder: computeIsElementAtAxis('start', 'y', props`worldObjectId`, props`sceneDataPath`),
+  isAtBottomBorder: computeIsElementAtAxis('end', 'y', props`worldObjectId`, props`sceneDataPath`),
+  isAtLeftBorder: computeIsElementAtAxis('start', 'x', props`worldObjectId`, props`sceneDataPath`),
+  isAtRightBorder: computeIsElementAtAxis('end', 'x', props`worldObjectId`, props`sceneDataPath`),
   tileSize: state`${props`sceneDataPath`}.viewport.tileSize`,
   worldObjectSelected: signal`worldObjectSelected`
 }, WorldObject);
@@ -34,9 +41,24 @@ function WorldObject(props) {
   );
 };
 
-function className({ isSelected }) {
+function className({ isSelected, isAtTopBorder, isAtBottomBorder, isAtLeftBorder, isAtRightBorder }) {
   if (isSelected) {
-    return 'world-object-border-color';
+    return map(
+      filter(
+        map(
+          {
+            'world-object-border-color': true,
+            'is-at-top-border': isAtTopBorder,
+            'is-at-bottom-border': isAtBottomBorder,
+            'is-at-left-border': isAtLeftBorder,
+            'is-at-right-border': isAtRightBorder,
+          },
+          function (apply, name) { return { name, apply } }
+        ),
+        'apply',
+      ),
+      'name'
+    ).join(' ');
   } else {
     return 'world-object-border-color-on-hover';
   }
