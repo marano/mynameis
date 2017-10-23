a0304908ba5ea4da2c9436a3d707c38704f1f7d2
marano <thiagomarano@gmail.com>
2017-10-23 00:55:48 -0200

    Lint compute.

--- js/computes.js
+++ js/computes.js
@@ -1,9 +1,9 @@
-import { compute } from 'cerebral';
-import { props, state } from 'cerebral/tags';
-import { range } from 'lodash';
-import { cross } from 'd3-array';
+import { compute } from "cerebral"
+import { state } from "cerebral/tags"
+import { range } from "lodash"
+import { cross } from "d3-array"
 
-import indexOfTileAt from './index-of-tile-at';
+import indexOfTileAt from "./index-of-tile-at"
 
 export function computeVisibleTileIndexes(sceneDataPath) {
   return compute(
@@ -14,40 +14,54 @@ export function computeVisibleTileIndexes(sceneDataPath) {
     state`${sceneDataPath}.size.x`,
     state`${sceneDataPath}.size.y`,
     state`editor.currentGameMode`,
-    function (viewportPositionX, viewportPositionY, viewportSizeX, viewportSizeY, sceneSizeX, sceneSizeY, currentGameMode) {
-      const animationOffset = currentGameMode == "play" ? 2 : 0;
-      const minX = Math.max(0, viewportPositionX - animationOffset);
-      const minY = Math.max(0, viewportPositionY - animationOffset);
+    function(
+      viewportPositionX,
+      viewportPositionY,
+      viewportSizeX,
+      viewportSizeY,
+      sceneSizeX,
+      sceneSizeY,
+      currentGameMode
+    ) {
+      const animationOffset = currentGameMode === "play" ? 2 : 0
+      const minX = Math.max(0, viewportPositionX - animationOffset)
+      const minY = Math.max(0, viewportPositionY - animationOffset)
 
-      const maxX = Math.min(viewportPositionX + viewportSizeX + animationOffset, sceneSizeX);
-      const maxY = Math.min(viewportPositionY + viewportSizeY + animationOffset, sceneSizeY);
+      const maxX = Math.min(
+        viewportPositionX + viewportSizeX + animationOffset,
+        sceneSizeX
+      )
+      const maxY = Math.min(
+        viewportPositionY + viewportSizeY + animationOffset,
+        sceneSizeY
+      )
 
-      var xRange = range(minX, maxX);
-      var yRange = range(minY, maxY);
+      var xRange = range(minX, maxX)
+      var yRange = range(minY, maxY)
 
-      return cross(xRange, yRange, (x, y) => indexOfTileAt(sceneSizeY, x, y));
+      return cross(xRange, yRange, (x, y) => indexOfTileAt(sceneSizeY, x, y))
     }
-  );
+  )
 }
 
 export function computeSelectedWorldObject(sceneDataPath) {
-  return compute(
-    state`${sceneDataPath}.selectedWorldObjectId`,
-    function (worldObjectId, get) {
-      return get(state`${sceneDataPath}.worldObjects.${worldObjectId}`);
-    }
-  );
+  return compute(state`${sceneDataPath}.selectedWorldObjectId`, function(
+    worldObjectId,
+    get
+  ) {
+    return get(state`${sceneDataPath}.worldObjects.${worldObjectId}`)
+  })
 }
 
 export const computeWorldObjectSelectable = compute(
   state`objectPicker.selectedEntityIndex`,
-  function (selectedEntityIndex) {
-    return !selectedEntityIndex;
+  function(selectedEntityIndex) {
+    return !selectedEntityIndex
   }
-);
+)
 
 export function computeSelectedTile(sceneDataPath) {
   return compute(
     state`${sceneDataPath}.tiles.${state`editor.selectedTileIndex`}`
-  );
+  )
 }
