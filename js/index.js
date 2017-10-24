8eafce141e97d69d89919d1748412eb2e0dd1b1d
marano <thiagomarano@gmail.com>
2017-10-24 00:16:46 -0200

    Style index.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,28 @@
-import { throttle } from 'lodash';
+import controller from "./controller"
 
-import controller from './controller';
+import { definitions as uiElements } from "../json/ui-elements.json"
+import { definitions as entities } from "../json/entities.json"
+import sceneTemplate from "../json/world.json"
 
-import { definitions as uiElements } from '../json/ui-elements.json';
-import { definitions as entities } from '../json/entities.json';
-import sceneTemplate from '../json/world.json';
+import Main from "./components/Main"
 
-import Main from './components/Main';
-
-controller.getSignal('entitiesLoaded')({ entities, uiElements });
-controller.getSignal('sceneTemplateLoaded')({ sceneTemplate });
+controller.getSignal("entitiesLoaded")({ entities, uiElements })
+controller.getSignal("sceneTemplateLoaded")({ sceneTemplate })
 
 window.addEventListener(
-  'keydown',
-  function (event) {
-    controller.getSignal('keyPressed')({
+  "keydown",
+  function(event) {
+    controller.getSignal("keyPressed")({
       key: event.key.toLowerCase(),
-      sceneDataPath: controller.getState('currentSceneDataPath')
-    });
+      sceneDataPath: controller.getState("currentSceneDataPath")
+    })
   },
   true
-);
+)
 
 View.render(
-  (
-    <Container controller={controller}>
-      <Main />
-    </Container>
-  ),
-  document.getElementById('root')
-);
+  <Container controller={controller}>
+    <Main />
+  </Container>,
+  document.getElementById("root")
+)
