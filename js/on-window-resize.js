28d84174320a3a1815a039f1fff33b4fbb6a6153
marano <thiagomarano@gmail.com>
2017-10-23 23:32:26 -0200

    Fix file style.

--- js/on-window-resize.js
+++ js/on-window-resize.js
@@ -1,25 +1,21 @@
-import { throttle, each } from 'lodash';
+import { throttle, each } from "lodash"
 
-const callbacks = {};
+const callbacks = {}
 
 const onWindowResize = throttle(
-  function () {
-    _.each(callbacks, function (callback, key) {
+  function() {
+    each(callbacks, function(callback, key) {
       if (callback) {
-        callback();
+        callback()
       }
-    });
+    })
   },
   50,
   { leading: false, trailing: true }
-);
+)
 
-window.addEventListener(
-  'resize',
-  onWindowResize,
-  true
-);
+window.addEventListener("resize", onWindowResize, true)
 
-export default function (key, callback) {
-  callbacks[key] = callback;
+export default function(key, callback) {
+  callbacks[key] = callback
 }
