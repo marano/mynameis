668f4f4c9b99855fbc8fba3e8f02db9ebbc6de9f
marano <thiagomarano@gmail.com>
2017-11-11 18:02:16 -0200

    Lint file.

--- bug.js
+++ bug.js
@@ -1,27 +1,27 @@
-import Inferno from 'inferno';
-import { Controller } from 'cerebral';
-import { Container, connect } from 'cerebral/inferno';
-import { state, props, signal } from 'cerebral/tags';
-import { set } from 'cerebral/operators';
+import Inferno from "inferno"
+import { Controller } from "cerebral"
+import { Container, connect } from "cerebral/inferno"
+import { state, props, signal } from "cerebral/tags"
+import { set } from "cerebral/operators"
 
 const controller = Controller({
   state: {
-    items: ['a', 'b', 'c', 'd'],
+    items: ["a", "b", "c", "d"],
     itemsIndexes: [0, 1, 2, 3]
   },
   signals: {
     keyPressedSignal: set(state`itemsIndexes`, [2, 3])
   }
-});
+})
 
 const ListItem = connect(
   {
     item: state`items.${props`itemIndex`}`
   },
-  function ListItem({ item  }) {
-    return <div>{item}</div>;
+  function ListItem({ item }) {
+    return <div>{item}</div>
   }
-);
+)
 
 const List = connect(
   {
@@ -33,17 +33,17 @@ const List = connect(
       <div>
         <button onclick={() => keyPressedSignal()}>Change list</button>
 
-        {itemsIndexes.map((itemIndex) => <ListItem key={itemIndex} itemIndex={itemIndex} />)}
+        {itemsIndexes.map(itemIndex => (
+          <ListItem key={itemIndex} itemIndex={itemIndex} />
+        ))}
       </div>
-    );
+    )
   }
-);
+)
 
 Inferno.render(
-  (
-    <Container controller={controller}>
-      <List />
-    </Container>
-  ),
-  document.getElementById('root')
-);
+  <Container controller={controller}>
+    <List />
+  </Container>,
+  document.getElementById("root")
+)
