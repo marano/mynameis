1b0a14631fdf30f3e130cf15fe6fc0dbf80554e8
marano <thiagomarano@gmail.com>
2018-01-27 10:01:27 -0200

    Refactor signals to sequences.

---
+++ js/sequences.js
@@ -0,0 +1,137 @@
+import { state, props } from "cerebral/tags"
+import { set, equals, push, unset, wait, when } from "cerebral/operators"
+import { isEqual } from "lodash/fp"
+
+import {
+  setEntitiesUiElements,
+  createScene,
+  fillSceneFromTemplate,
+  adjustViewportSize,
+  adjustViewportPositionForCameraMode,
+  handleKeyPress as handleKeyPressAction,
+  changeSceneSize as changeSceneSizeAction,
+  updateSortedTileIds,
+  addWorldObject as addWorldObjectAction,
+  makeSceneTemplateFromScene,
+  selectSceneTile as selectSceneTileAction,
+  moveViewport as moveViewportAction,
+  removeEditorScenePaths,
+  computeVisibleTileIds
+} from "./actions"
+
+export const loadEntities = [
+  setEntitiesUiElements,
+  set(state`definitions.entities`, props`entities`)
+]
+
+export const selectWorldObject = [
+  when(state`${props`scenePath`}.selectedWorldObjectId`),
+  {
+    true: set(
+      state`${props`scenePath`}.worldObjects.${state`${props`scenePath`}.selectedWorldObjectId`}.isSelected`,
+      false
+    ),
+    false: []
+  },
+  set(state`${props`scenePath`}.selectedWorldObjectId`, props`worldObjectId`),
+  set(
+    state`${props`scenePath`}.worldObjects.${state`${props`scenePath`}.selectedWorldObjectId`}.isSelected`,
+    true
+  )
+]
+
+export const selectObjectPickerEntity = set(
+  state`editor.objectPicker.selectedEntityIndex`,
+  props`entityIndex`
+)
+
+export const addWorldObject = addWorldObjectAction
+
+export const selectSceneTile = [
+  when(state`editor.selectedTilePath`),
+  {
+    true: set(state`${state`editor.selectedTilePath`}.isSelected`, false),
+    false: []
+  },
+  selectSceneTileAction,
+  set(state`${state`editor.selectedTilePath`}.isSelected`, true)
+]
+
+export const changeMode = [
+  equals(props`mode`),
+  {
+    game: [
+      makeSceneTemplateFromScene,
+      createScene,
+      fillSceneFromTemplate,
+      updateSortedTileIds,
+      set(state`${props`scenePath`}.currentMode`, "game"),
+      set(state`viewport.currentScenePath`, props`scenePath`),
+      set(state`${props`scenePath`}.viewport.cameraLockMode`, "locked"),
+      adjustViewportSize,
+      adjustViewportPositionForCameraMode,
+      computeVisibleTileIds
+    ],
+    editor: [
+      set(
+        state`viewport.currentScenePath`,
+        state`${props`scenePath`}.sourceScenePath`
+      ),
+      wait(0),
+      unset(state`${props`scenePath`}`)
+    ]
+  }
+]
+
+export const changeCameraMode = [
+  set(
+    state`${props`scenePath`}.viewport.cameraLockMode`,
+    props`cameraLockMode`
+  ),
+  adjustViewportSize,
+  adjustViewportPositionForCameraMode,
+  computeVisibleTileIds
+]
+
+export const changeSceneSize = [
+  changeSceneSizeAction,
+  updateSortedTileIds,
+  adjustViewportSize,
+  adjustViewportPositionForCameraMode,
+  computeVisibleTileIds
+]
+
+export const resizeViewport = [
+  set(state`viewport.containerDimension.width`, props`viewportWidth`),
+  set(state`viewport.containerDimension.height`, props`viewportHeight`),
+  adjustViewportSize,
+  computeVisibleTileIds
+]
+
+export const handleKeyPress = handleKeyPressAction
+
+export const moveViewport = [moveViewportAction, computeVisibleTileIds]
+
+export const addNewScene = [
+  createScene,
+  set(state`viewport.currentScenePath`, props`scenePath`),
+  push(state`editor.scenePaths`, props`scenePath`),
+  adjustViewportSize,
+  computeVisibleTileIds
+]
+
+export const changeScene = [
+  set(state`viewport.currentScenePath`, props`scenePath`),
+  adjustViewportSize,
+  computeVisibleTileIds
+]
+
+export const closeScene = [
+  when(props`scenePath`, state`viewport.currentScenePath`, isEqual),
+  {
+    true: [set(state`viewport.currentScenePath`, null)],
+    false: []
+  },
+  removeEditorScenePaths,
+  unset(state`scenePath`)
+]
--- js/signals.js
+++ js/signals.js
@@ -1,125 +1,33 @@
-import { state, props } from "cerebral/tags"
-import { set, equals, push, unset, wait, when } from "cerebral/operators"
-import { isEqual } from "lodash/fp"
-
 import {
-  setEntitiesUiElements,
-  createScene,
-  fillSceneFromTemplate,
-  adjustViewportSize,
-  adjustViewportPositionForCameraMode,
-  handleKeyPress,
-  changeSceneSize,
-  updateSortedTileIds,
+  loadEntities,
+  selectWorldObject,
+  selectObjectPickerEntity,
   addWorldObject,
-  makeSceneTemplateFromScene,
   selectSceneTile,
+  changeMode,
+  changeCameraMode,
+  changeSceneSize,
+  resizeViewport,
+  handleKeyPress,
   moveViewport,
-  removeEditorScenePaths,
-  computeVisibleTileIds
-} from "./actions"
+  addNewScene,
+  changeScene,
+  closeScene
+} from "./sequences"
 
 export default {
-  entitiesLoaded: [
-    setEntitiesUiElements,
-    set(state`definitions.entities`, props`entities`)
-  ],
-  worldObjectSelected: [
-    when(state`${props`scenePath`}.selectedWorldObjectId`),
-    {
-      true: set(
-        state`${props`scenePath`}.worldObjects.${state`${props`scenePath`}.selectedWorldObjectId`}.isSelected`,
-        false
-      ),
-      false: []
-    },
-    set(state`${props`scenePath`}.selectedWorldObjectId`, props`worldObjectId`),
-    set(
-      state`${props`scenePath`}.worldObjects.${state`${props`scenePath`}.selectedWorldObjectId`}.isSelected`,
-      true
-    )
-  ],
-  objectPickerEntitySelected: [
-    set(state`editor.objectPicker.selectedEntityIndex`, props`entityIndex`)
-  ],
-  worldObjectAdded: [addWorldObject],
-  sceneTileSelected: [
-    when(state`editor.selectedTilePath`),
-    {
-      true: set(state`${state`editor.selectedTilePath`}.isSelected`, false),
-      false: []
-    },
-    selectSceneTile,
-    set(state`${state`editor.selectedTilePath`}.isSelected`, true)
-  ],
-  modeChanged: [
-    equals(props`mode`),
-    {
-      game: [
-        makeSceneTemplateFromScene,
-        createScene,
-        fillSceneFromTemplate,
-        updateSortedTileIds,
-        set(state`${props`scenePath`}.currentMode`, "game"),
-        set(state`viewport.currentScenePath`, props`scenePath`),
-        set(state`${props`scenePath`}.viewport.cameraLockMode`, "locked"),
-        adjustViewportSize,
-        adjustViewportPositionForCameraMode,
-        computeVisibleTileIds
-      ],
-      editor: [
-        set(
-          state`viewport.currentScenePath`,
-          state`${props`scenePath`}.sourceScenePath`
-        ),
-        wait(0),
-        unset(state`${props`scenePath`}`)
-      ]
-    }
-  ],
-  cameraModeChanged: [
-    set(
-      state`${props`scenePath`}.viewport.cameraLockMode`,
-      props`cameraLockMode`
-    ),
-    adjustViewportSize,
-    adjustViewportPositionForCameraMode,
-    computeVisibleTileIds
-  ],
-  sceneSizeChanged: [
-    changeSceneSize,
-    updateSortedTileIds,
-    adjustViewportSize,
-    adjustViewportPositionForCameraMode,
-    computeVisibleTileIds
-  ],
-  viewportResized: [
-    set(state`viewport.containerDimension.width`, props`viewportWidth`),
-    set(state`viewport.containerDimension.height`, props`viewportHeight`),
-    adjustViewportSize,
-    computeVisibleTileIds
-  ],
+  entitiesLoaded: loadEntities,
+  worldObjectSelected: selectWorldObject,
+  objectPickerEntitySelected: selectObjectPickerEntity,
+  worldObjectAdded: addWorldObject,
+  sceneTileSelected: selectSceneTile,
+  modeChanged: changeMode,
+  cameraModeChanged: changeCameraMode,
+  sceneSizeChanged: changeSceneSize,
+  viewportResized: resizeViewport,
   keyPressed: handleKeyPress,
-  viewportMoved: [moveViewport, computeVisibleTileIds],
-  newSceneAdded: [
-    createScene,
-    set(state`viewport.currentScenePath`, props`scenePath`),
-    push(state`editor.scenePaths`, props`scenePath`),
-    adjustViewportSize,
-    computeVisibleTileIds
-  ],
-  sceneChanged: [
-    set(state`viewport.currentScenePath`, props`scenePath`),
-    adjustViewportSize,
-    computeVisibleTileIds
-  ],
-  sceneClosed: [
-    when(props`scenePath`, state`viewport.currentScenePath`, isEqual),
-    {
-      true: [set(state`viewport.currentScenePath`, null)],
-      false: []
-    },
-    removeEditorScenePaths,
-    unset(state`scenePath`)
-  ]
+  viewportMoved: moveViewport,
+  newSceneAdded: addNewScene,
+  sceneChanged: changeScene,
+  sceneClosed: closeScene
 }
