b15e43de1be1e5a07c3d47287416e8c6d0f12c55
marano <thiagomarano@gmail.com>
2018-02-07 23:19:46 -0200

    Local storage state synchronization.

--- js/components/GlobalListeners.js
+++ js/components/GlobalListeners.js
@@ -3,10 +3,13 @@ import { inject } from "mobx-react"
 
 import keydownStream from "../streams/keydown"
 import createMoveKeydownStream from "../streams/moveKeydown"
+import createMutationStream from "../streams/mutation"
+
 import createKeydownEventHandler from "../event-handlers/keydown"
 import createMoveKeydownEventHandler, {
   movementKeys
 } from "../event-handlers/moveKeydown"
+import createMutationEventHandler from "../event-handlers/mutation"
 
 export default inject("state", "actions")(
   class GlobalListeners extends Component {
@@ -20,11 +23,16 @@ export default inject("state", "actions")(
       ).subscribe(
         createMoveKeydownEventHandler(this.props.state, this.props.actions)
       )
+
+      this.mutationSubscription = createMutationStream(
+        this.props.state
+      ).subscribe(createMutationEventHandler())
     }
 
     componentWillUnmount() {
       this.keydownSubscription.unsubscribe()
       this.moveKeydownSubscription.unsubscribe()
+      this.mutationSubscription.unsubscribe()
     }
 
     render() {
--- js/streams/mutation.js
+++ js/streams/mutation.js
@@ -1,8 +1,8 @@
-import { autorunAsync, toJS } from "mobx"
+import { autorunAsync } from "mobx"
 import { Subject } from "rxjs"
 
-export default function createOnMutationStream(store) {
+export default function createOnMutationStream(state) {
   var observable = new Subject()
-  const dispose = autorunAsync(() => observable.next(toJS(store)), 1000)
+  const dispose = autorunAsync(() => observable.next(state), 1000)
   return observable.finally(dispose)
 }
